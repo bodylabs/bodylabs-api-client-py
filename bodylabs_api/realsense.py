a34a5d9021b0e4e30839b9c5ef86bd1251548d15
Martin Kristiansen <martin.kristiansen@bodylabs.com>
2016-06-06 17:34:47 -0400

    Changed order if properties

--- bodylabs_api/realsense.py
+++ bodylabs_api/realsense.py
@@ -12,6 +12,10 @@ class RealsenseInput(Input):
         return self._cached_artifact('RealsenseMeasurements', 'valuesJson')
 
     @property
+    def measured_mesh(self):
+        return self._cached_artifact('RealsenseMeasurements', 'measuredMesh')
+
+    @property
     def matched_body(self):
         return self._cached_artifact('RealsenseMatchedBody', 'matchInfoJson')
 
@@ -19,6 +23,4 @@ class RealsenseInput(Input):
     def matched_body_m2m_leg(self):
         return self._cached_artifact('RealsenseMatchedBody', 'matchInfoJsonM2mLeg')
 
-    @property
-    def measured_mesh(self):
-        return self._cached_artifact('RealsenseMeasurements', 'measuredMesh')
+
