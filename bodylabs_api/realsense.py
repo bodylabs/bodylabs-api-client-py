d66d344c5e68e1275046ac6c3dcc8cf763e69ab8
Martin Kristiansen <martin.kristiansen@bodylabs.com>
2016-06-07 17:12:19 -0400

    Old ds4 artifact names, new ones are not deployed

--- bodylabs_api/realsense.py
+++ bodylabs_api/realsense.py
@@ -5,22 +5,22 @@ class RealsenseInput(Input):
 
     @property
     def fused_alignment(self):
-        return self._cached_artifact('RealsenseAlignment', 'canonicalAlignment')
+        return self._cached_artifact('ds4Alignment', 'canonicalAlignment')
 
     @property
     def measurements(self):
-        return self._cached_artifact('RealsenseMeasurements', 'valuesJson')
+        return self._cached_artifact('ds4Measurements', 'valuesJson')
 
     @property
     def measured_mesh(self):
-        return self._cached_artifact('RealsenseMeasurements', 'measuredMesh')
+        return self._cached_artifact('ds4Measurements', 'measuredMesh')
 
     @property
     def matched_body(self):
-        return self._cached_artifact('RealsenseMatchedBody', 'matchInfoJson')
+        return self._cached_artifact('ds4MatchedBody', 'matchInfoJson')
 
     @property
     def matched_body_m2m_leg(self):
-        return self._cached_artifact('RealsenseMatchedBody', 'matchInfoJsonM2mLeg')
+        return self._cached_artifact('ds4MatchedBody', 'matchInfoJsonM2mLeg')
 
 
